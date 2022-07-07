6a6f65cc0f62e11244a6ca4dc94553801e688843
dtbuchholz <buchhlz2@gmail.com>
2022-07-06 23:11:31 -0500

    feat: add NFT metadata upload & deploy locally - Finished `uploadMetadataToIpfs.js`, which takes multiple files (assumes matching prefix, `id`), uploads the `assets` files to IPFS, takes that CID, inserts into the `metadta` that matches on the same `id`, and then takes this entire `metadata` directory and pushed it to IPFS (via NFT.Storage) - Updated the `TableNFT` smart contract to allow for `baseURI` to be set -- and some public getters for simplicity sake - Updated `test.js` to walk through some basic tests on `TableNFT`, such as initialization, minting, and `tokenURI` validation with the passed IPFS directory CID

--- contracts/TableNFT.sol
+++ contracts/TableNFT.sol
@@ -4,18 +4,34 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TableNFT is ERC721 {
-    uint256 public tokenCounter;
+    // A URI used to reference off-chain metadata (set `public`, for now)
+    string public baseURIString;
+    // A token countr, to track NFT `tokenId` (set `public`, for now)
+    uint256 public tokenId;
 
-    constructor() ERC721("TableNFT", "TNFT") {
-        tokenCounter = 0;
+    /**
+        @dev Initialize TableNFT
+        @param baseURI Set the contract's base URI to the IPFS directory CID
+     */
+    constructor(string memory baseURI) ERC721("TableNFT", "TNFT") {
+        // Initialize with token counter at zero
+        tokenId = 0;
+        // Set the base URI
+        baseURIString = baseURI;
     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "{TODO_ipfs_gateway_to_directory_cid}";
+    /**
+        @dev Must override the default implementation, which returns an empty string
+     */
+    function _baseURI() internal view override returns (string memory) {
+        return baseURIString;
     }
 
+    /** 
+        @dev Mint an NFT, incrementing the `tokenId` upon each call
+     */
     function mint() public {
-        _safeMint(msg.sender, tokenCounter);
-        tokenCounter++;
+        _safeMint(msg.sender, tokenId);
+        tokenId++;
     }
 }
